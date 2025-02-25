844d316646e06f3248fac584cc135be4b277b871
severogianluca <gianlucasevero300@gmail.com>
2025-02-25 17:18:24 +0100

    Feat: create lista e fizzBuzz

--- js/app.js
+++ js/app.js
@@ -2,6 +2,14 @@
 // CREAZIONE CICLO CHE STAMPI NUMERI DA 1 A 100
 for(let number = 1; number <= 100; number++){
 
+    // creo una serie di condinzioni in modo da stampare i diversi multipli
+    // dichiaro subito la condizione FizzBuzz perchè sennò non arriverà mai questa condizione
+    // avendo prima multipli 3 e 5 uscirà dal blocco non arrivando mai a fizzbuzz
+    if(number % 3 === 0 && number % 5 === 0){
+        console.log("FizzBuzz")
+    }else{
+        console.log(number)
+    }
 }
 
 
