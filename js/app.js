fe9d1972c7051e9c90c3c7493bf6dcfe13d22a5d
severogianluca <gianlucasevero300@gmail.com>
2025-02-25 17:46:53 +0100

    fix

--- js/app.js
+++ js/app.js
@@ -6,13 +6,13 @@ for(let number = 1; number <= 100; number++){
     // dichiaro subito la condizione FizzBuzz perchè sennò non arriverà mai questa condizione
     // avendo prima multipli 3 e 5 uscirà dal blocco non arrivando mai a fizzbuzz
     if(number % 3 === 0 && number % 5 === 0){
-        console.log("FizzBuzz")
+        console.log("FizzBuzz");
     }else if(number % 3 === 0){
-        console.log("Fizz")
+        console.log("Fizz");
     }else if(number % 5 === 0){
-        console.log("Buzz")
+        console.log("Buzz");
     }else{
-        console.log(number)
+        console.log(number);
     }
 }
 
